8cd45237850e24dbfd3dd4c2f520b240a5dffd2a
sebpy <sebastien.fantinati@gmail.com>
2019-09-30 18:33:58 +0200

    change settings.py. add os.path in DIRS

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Before: 'DIRS': ['templates']
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,6 +79,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
