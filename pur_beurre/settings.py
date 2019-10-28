563d18ca5b4899ba06d8e6807a7a97d203bbe018
sebpy <sebastien.fantinati@gmail.com>
2019-10-28 19:01:33 +0100

    Many fix for travis

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 from django.contrib.messages import constants as messages
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
